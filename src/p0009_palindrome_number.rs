259232cde608c04bf73ca9ac68df0af7754e3138
Yujie Xia <xiayjchn@gmail.com>
2020-11-23 00:33:58 +0800

    simplify palindrome

--- src/p0009_palindrome_number.rs
+++ src/p0009_palindrome_number.rs
@@ -6,24 +6,13 @@ impl Solution {
         if x < 0 {
             return false;
         }
-        if x == 0 {
-            return true;
+        let mut t = x;
+        let mut rx = 0;
+        while t > 0 {
+            rx = rx * 10 + t % 10;
+            t /= 10;
         }
-        let mut x = x;
-        let mut digits: Vec<i32> = Vec::new();
-        while x > 0 {
-            digits.push(x % 10);
-            x /= 10;
-        }
-        let (mut i, mut j) = (0, digits.len() - 1);
-        while i < j {
-            if digits[i] != digits[j] {
-                return false;
-            }
-            i += 1;
-            j -= 1;
-        }
-        return true;
+        return rx == x;
     }
 }
 
