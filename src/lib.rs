92ad911e0d843705660867e84181be8f07dada00
Yujie Xia <xiayjchn@gmail.com>
2021-02-02 23:45:15 +0800

    solved problem 202

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ mod util;
 mod archived;
 
 mod p0201_bitwise_and_of_numbers_range;
+mod p0202_happy_number;
---
+++ src/p0202_happy_number.rs
@@ -0,0 +1,39 @@
+#![allow(dead_code)]
+pub struct Solution {}
+
+use std::collections::HashSet;
+
+impl Solution {
+    fn dfs(n: i32, visited: &mut HashSet<i32>) -> bool {
+        if n == 1 {
+            return true;
+        }
+        visited.insert(n);
+        let mut sum = 0;
+        let mut n = n;
+        while n > 0 {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        if visited.contains(&sum) {
+            return false;
+        }
+        Solution::dfs(sum, visited)
+    }
+
+    pub fn is_happy(n: i32) -> bool {
+        let mut visited = HashSet::new();
+        Solution::dfs(n, &mut visited)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_happy() {
+        assert_eq!(Solution::is_happy(19), true);
+        assert_eq!(Solution::is_happy(2), false);
+    }
+}
