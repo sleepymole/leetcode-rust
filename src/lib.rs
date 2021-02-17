400c4638af0e98f12234f224e732d3da303bc50c
Yujie Xia <xiayjchn@gmail.com>
2021-02-17 23:54:17 +0800

    solved problem 219

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,4 @@ mod p0214_shortest_palindrome;
 mod p0215_kth_largest_element_in_an_array;
 mod p0216_combination_sum_iii;
 mod p0217_contains_duplicate;
+mod p0219_contains_duplicate_ii;
---
+++ src/p0219_contains_duplicate_ii.rs
@@ -0,0 +1,40 @@
+#![allow(dead_code)]
+pub struct Solution {}
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut m = HashMap::new();
+        for (i, x) in nums.iter().enumerate() {
+            if let Some(j) = m.get(x) {
+                if i - j <= k as usize {
+                    return true;
+                }
+            }
+            m.insert(x, i);
+        }
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_contains_nearby_duplicate() {
+        assert_eq!(
+            Solution::contains_nearby_duplicate(vec![1, 2, 3, 1], 3),
+            true
+        );
+        assert_eq!(
+            Solution::contains_nearby_duplicate(vec![1, 0, 1, 1], 1),
+            true
+        );
+        assert_eq!(
+            Solution::contains_nearby_duplicate(vec![1, 2, 3, 1, 2, 3], 2),
+            false
+        );
+    }
+}
