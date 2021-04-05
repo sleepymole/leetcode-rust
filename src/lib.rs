17791fa42399ee87ae62302b9087e1adbdef358f
Yujie Xia <xiayjchn@gmail.com>
2021-04-05 23:47:48 +0800

    solved problem 331

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ mod p0327_count_of_range_sum;
 mod p0328_odd_even_linked_list;
 mod p0329_longest_increasing_path_in_a_matrix;
 mod p0330_patching_array;
+mod p0331_verify_preorder_serialization_of_a_binary_tree;
 mod p0334_increasing_triplet_subsequence;
 mod p0342_power_of_four;
 mod p0344_reverse_string;
---
+++ src/p0331_verify_preorder_serialization_of_a_binary_tree.rs
@@ -0,0 +1,46 @@
+#![allow(dead_code)]
+pub struct Solution;
+
+impl Solution {
+    pub fn is_valid_serialization(preorder: String) -> bool {
+        let mut n = 0;
+        let mut ok = false;
+        for s in preorder.rsplit(',') {
+            if s == "#" {
+                n += 1;
+                if n == 1 {
+                    ok = true;
+                }
+            } else if s.parse::<i32>().ok().is_some() {
+                if n >= 2 {
+                    n -= 1;
+                    ok = true;
+                } else {
+                    n += 1;
+                    ok = false
+                }
+            } else {
+                return false;
+            }
+        }
+        ok && n == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid_serialization() {
+        assert_eq!(
+            Solution::is_valid_serialization("9,3,4,#,#,1,#,#,2,#,6,#,#".to_owned()),
+            true
+        );
+        assert_eq!(Solution::is_valid_serialization("1,#".to_owned()), false);
+        assert_eq!(
+            Solution::is_valid_serialization("9,#,#,1".to_owned()),
+            false
+        );
+    }
+}
