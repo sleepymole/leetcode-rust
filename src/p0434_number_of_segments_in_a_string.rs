f3a1dcf02393c02658980169211a36d7c4540420
Yujie Xia <xiayjchn@gmail.com>
2021-05-22 22:28:31 +0800

    solved problem 434

---
+++ src/p0434_number_of_segments_in_a_string.rs
@@ -0,0 +1,34 @@
+#![allow(dead_code)]
+pub struct Solution;
+
+impl Solution {
+    pub fn count_segments(s: String) -> i32 {
+        let mut segs = 0;
+        let mut inword = false;
+        for c in s.chars() {
+            if c == ' ' {
+                inword = false;
+            } else {
+                if !inword {
+                    segs += 1;
+                    inword = true;
+                }
+            }
+        }
+        segs
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_segments() {
+        assert_eq!(
+            Solution::count_segments("Hello, my name is John".to_owned()),
+            5
+        );
+        assert_eq!(Solution::count_segments("Hello".to_owned()), 1);
+    }
+}
