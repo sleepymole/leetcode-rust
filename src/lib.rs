95ea6ffcabe5588d2524cbd2a292733566cf11c8
Yujie Xia <xiayjchn@gmail.com>
2021-07-04 23:17:58 +0800

    solved problem 468

--- src/lib.rs
+++ src/lib.rs
@@ -54,4 +54,5 @@ mod p0463_island_perimeter;
 mod p0464_can_i_win;
 mod p0466_count_the_repetitions;
 mod p0467_unique_substrings_in_wraparound_string;
+mod p0468_validate_ip_address;
 mod p0476_number_complement;
---
+++ src/p0468_validate_ip_address.rs
@@ -0,0 +1,76 @@
+#![allow(dead_code)]
+pub struct Solution;
+
+impl Solution {
+    fn valid_ipv4(ip: &Vec<char>) -> bool {
+        let fields: Vec<&[char]> = ip.split(|&c| c == '.').collect();
+        if fields.len() != 4 {
+            return false;
+        }
+        for field in fields {
+            if field.is_empty() || field.len() > 1 && field[0] == '0' || field.len() > 3 {
+                return false;
+            }
+            let mut x = 0;
+            for &c in field {
+                x = x * 10 + c as i32 - '0' as i32;
+            }
+            if x >= 256 {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn valid_ipv6(ip: &Vec<char>) -> bool {
+        let fields: Vec<&[char]> = ip.split(|&c| c == ':').collect();
+        if fields.len() != 8 {
+            return false;
+        }
+        for field in fields {
+            if field.is_empty() || field.len() > 4 {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn valid_ip_address(ip: String) -> String {
+        let ip: Vec<char> = ip.chars().collect();
+        if ip.iter().all(|&c| c.is_digit(10) || c == '.') && Solution::valid_ipv4(&ip) {
+            return "IPv4".to_owned();
+        } else if ip.iter().all(|&c| c.is_digit(16) || c == ':') && Solution::valid_ipv6(&ip) {
+            return "IPv6".to_owned();
+        }
+        "Neither".to_owned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_ip_address() {
+        assert_eq!(
+            Solution::valid_ip_address("172.16.254.1".to_owned()),
+            "IPv4".to_owned()
+        );
+        assert_eq!(
+            Solution::valid_ip_address("2001:0db8:85a3:0:0:8A2E:0370:7334".to_owned()),
+            "IPv6".to_owned()
+        );
+        assert_eq!(
+            Solution::valid_ip_address("256.256.256.256".to_owned()),
+            "Neither".to_owned()
+        );
+        assert_eq!(
+            Solution::valid_ip_address("2001:0db8:85a3:0:0:8A2E:0370:7334:".to_owned()),
+            "Neither".to_owned()
+        );
+        assert_eq!(
+            Solution::valid_ip_address("1e1.4.5.6".to_owned()),
+            "Neither".to_owned()
+        );
+    }
+}
